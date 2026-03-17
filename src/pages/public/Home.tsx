b3bebaebb288641071acee5ca1ed16aa6654e507
clivon254 <clivon254@icloud.com>
2026-03-17 10:35:40 +0300

    Refactor Home component to import Link from react-router-dom and remove unused FaArrowRight import. This streamlines the code and improves readability.

--- src/pages/public/Home.tsx
+++ src/pages/public/Home.tsx
@@ -1,10 +1,9 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import HeroImage from "../../assets/Hero.jpeg";
 import { useGetAllProducts } from "../../tanstack/useProducts";
 import ProductCard from "../../components/product/ProductCard";
 import ProductCardSkeleton from "../../components/product/ProductCardSkeleton";
-import {Link} from "react-router-dom"
-import { FaArrowRight } from "react-icons/fa";
 
 const Home = () => {
   const [windowSize, setWindowSize] = useState({
